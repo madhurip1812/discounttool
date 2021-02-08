cd609658026bcd89fa62479936cbb02dbf950d73
Poonam Abhang <poonam.abhang@firstcry.com>
2021-02-08 15:11:30 +0530

    merge

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 //Route::any('/admin/{username?}/{password?}/{date?}/{countrycode?}/{langauge?}','UserLoginController@index')->name('userlogin');
 Route::get('/logout','UserLoginController@logout')->name('logout');
-
 Route::any('/addcashback','CashbackController@addcashback')->name('addcashback');
 Route::any('/admin/{id?}','CashbackController@main')->name('main');
 Route::any('/excludeitems','CouponController@excludeitems')->name('excludeitems');
