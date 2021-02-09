2bbfc9145c63ce85ee565c548e93baa741eb8993
madhuri patil <madhuri.patil@firstcry.com>
2021-02-09 20:07:51 +0530

    09feb21

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -11,11 +11,18 @@ use App\Models\SessionCountryModel;
 
 class UserLoginController extends Controller
 {
-    public function index($username='',$password='',$date='',$countrycode='',$langauge='') {
+    public function index($parameters='') {
     	//echo base64_encode($username) . " " . base64_encode($password) . " " . base64_encode($date) . " " . base64_encode($countrycode) . " " .base64_encode($langauge);exit;  cmVraGFAZ21haWwuY29t cmVraGFAMTIz MjAyMS0wMS0yOA== dWFl ZW5n rekha@gmail.com/rekha@123/2021-01-28/uae/eng
-        $username = base64_decode($username);
-        $password = base64_decode($password);
-        $date = base64_decode($date);
+        $decodedParams = base64_decode($parameters);
+        $decodedParams = explode('::',$decodedParams);
+        $emailaddress = $decodedParams[0];
+        $password = $decodedParams[1];
+        $date = $decodedParams[2];
+        $langauge = $decodedParams[3];
+        $pageid = $decodedParams[4];
+        $accesskey = $decodedParams[5];
+        $secretkey = $decodedParams[6];
+
         $countrycode = base64_decode($countrycode);
         $langauge = base64_decode($langauge);
         $encData = $username.'::'.$password.'::'.$date.'::'.$countrycode.'::'.$langauge.'::'.$pageid;
